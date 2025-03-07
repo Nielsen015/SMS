32bc59336da84215224e04e98ab0ebfde155116a
Moses Nielsen <mosesnielsen68@gmail.com>
2025-03-07 03:13:59 +0300

    refactor AttendanceChart component to use BarChart; update data structure and improve layout

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import Image from "next/image";
 import {
-  LineChart,
-  Line,
+  BarChart,
+  Bar,
+  Rectangle,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -14,112 +14,73 @@ import {
 
 const data = [
   {
-    name: "Jan",
-    income: 4000,
-    expense: 2400,
+    name: "Mon",
+    present: 60,
+    absent: 40,
   },
   {
-    name: "Feb",
-    income: 3000,
-    expense: 1398,
+    name: "Tue",
+    present: 70,
+    absent: 60,
   },
   {
-    name: "Mar",
-    income: 2000,
-    expense: 9800,
+    name: "Wed",
+    present: 90,
+    absent: 75,
   },
   {
-    name: "Apr",
-    income: 2780,
-    expense: 3908,
+    name: "Thu",
+    present: 90,
+    absent: 75,
   },
   {
-    name: "May",
-    income: 1890,
-    expense: 4800,
-  },
-  {
-    name: "Jun",
-    income: 2390,
-    expense: 3800,
-  },
-  {
-    name: "Jul",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Aug",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Sep",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Oct",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Nov",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Dec",
-    income: 3490,
-    expense: 4300,
+    name: "Fri",
+    present: 65,
+    absent: 55,
   },
 ];
 
-const FinanceChart = () => {
+const AttendanceChart = () => {
   return (
-    <div className="bg-white rounded-xl w-full h-full p-4">
+    <div className="bg-white rounded-lg p-4 h-full">
       <div className="flex justify-between items-center">
-        <h1 className="text-lg font-semibold">Finance</h1>
+        <h1 className="text-lg font-semibold">Attendance</h1>
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
       <ResponsiveContainer width="100%" height="90%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
           <XAxis
             dataKey="name"
             axisLine={false}
             tick={{ fill: "#d1d5db" }}
             tickLine={false}
-            tickMargin={10}
           />
-          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false}  tickMargin={20}/>
-          <Tooltip />
+          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
           <Legend
-            align="center"
+            align="left"
             verticalAlign="top"
-            wrapperStyle={{ paddingTop: "10px", paddingBottom: "30px" }}
+            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
           />
-          <Line
-            type="monotone"
-            dataKey="income"
-            stroke="#C3EBFA"
-            strokeWidth={5}
+          <Bar
+            dataKey="present"
+            fill="#FAE27C"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
           />
-          <Line type="monotone" dataKey="expense" stroke="#CFCEFF" strokeWidth={5}/>
-        </LineChart>
+          <Bar
+            dataKey="absent"
+            fill="#C3EBFA"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
       </ResponsiveContainer>
     </div>
   );
 };
 
-export default FinanceChart;
+export default AttendanceChart;
