44e5a67f44b180355fcd7fb0ecf7da6f438df279
Moses Nielsen <mosesnielsen68@gmail.com>
2025-03-08 01:53:41 +0300

    enhance DashboardLayout styles; add no-scrollbar class and adjust height for better layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
+    <div className="h-screen flex no-scrollbar ">
       {/* LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
@@ -22,7 +22,7 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll no-scrollbar flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] h-full overflow-scroll no-scrollbar bg-[#F7F8FA] flex flex-col">
         <Navbar />
         {children}
       </div>
