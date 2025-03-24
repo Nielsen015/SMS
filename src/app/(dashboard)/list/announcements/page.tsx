5182d7e119f2d9a429ac9d2bc2fda9a3713e2a26
Moses Nielsen <mosesnielsen68@gmail.com>
2025-03-25 01:23:34 +0300

    lists for announcements

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,39 +1,29 @@
 import Pagination from "@/components/Pagination"
 import Tables from "@/components/Tables"
 import TableSearch from "@/components/TableSearch"
-import {resultsData, role } from "@/lib/data"
+import {announcementsData, role } from "@/lib/data"
 import Image from 'next/image'
 import Link from "next/link"
 
-type Result = {
+type Announcement = {
   id:number;
-  subject:string;
-  student:string;
-  score:number;
-  teacher:string;
+  title:string;
   class:string;
   date:string;
-
 }
 const columns =[
-  {header:'Subject',accessor:'subject'},
-  {header:'Student',accessor:'student'},
-  {header:'Score %',accessor:'score',className:'hidden md:table-cell'},
-  {header:'Teacher',accessor:'teacher',className:'hidden md:table-cell'},
-  {header:'Class',accessor:'class',className:'hidden md:table-cell'},
+  {header:'Title',accessor:'title'},
+  {header:'Class',accessor:'class'},
   {header:'Date',accessor:'date',className:'hidden md:table-cell'},
   {header:'Actions',accessor:'action'},
 ]
-const ResultsListPage = () => {
-  const renderRow = (item:Result)=>(
+const AnnouncementsListPage = () => {
+  const renderRow = (item:Announcement)=>(
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight">
       <td className="flex items-center gap-4 p-4">
-      {item.subject}
+      {item.title}
       </td>
-      <td>{item.student}</td>
-      <td className="hidden md:table-cell">{item.score}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.class}</td>
+      <td>{item.class}</td>
       <td className="hidden md:table-cell">{item.date}</td>
       <td>
         <div className='flex items-center gap-2'>
@@ -54,7 +44,7 @@ const ResultsListPage = () => {
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
       {/* TOP */}
       <div className='flex items-center justify-between'>
-        <h1 className="hidden md:block text-lg font-semibold">All Results</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Announcements</h1>
         <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
           <TableSearch />
           <div className='flex items-center gap-4 self-end'>
@@ -71,11 +61,11 @@ const ResultsListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Tables columns={columns} renderRow={renderRow} data={resultsData}/>
+      <Tables columns={columns} renderRow={renderRow} data={announcementsData}/>
       {/* Pagination */}
         <Pagination />
     </div>
   )
 }
 
-export default ResultsListPage
+export default AnnouncementsListPage
---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,21 @@
+const SingleTeacherPage = () => {
+  return (
+    <div className='flex-1 p-4 flex flex-col gap-4 xl-flex-row'>
+        {/* LEFT */}
+       <div className='w-full xl:w-2/3'>
+       {/* TOP */}
+       <div className=''>
+        {/* User info card */}
+       </div>
+       <div className=''>
+        {/* Small card */}
+       </div>
+            
+       </div>
+       {/* RIGHT */}
+       <div className='w-full xl:w-1/3'></div>
+    </div>
+  )
+}
+
+export default SingleTeacherPage
