becc8abaaf343e10db53917b2e701192b908cce4
Moses Nielsen <mosesnielsen68@gmail.com>
2025-03-29 05:39:29 +0300

    Model created in Prisma ORM; update FormModal for better responsiveness

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -107,7 +107,7 @@ const FormModal = ({table,type,data,id}:{
     <Image src={`/${type}.png`} alt='' width={16} height={16} />
     </button>
     {open && <div className='w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center'>
-        <div className='bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]'>
+        <div className='bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] max-h-[calc(100vh+2rem)] overflow-y-auto flex items-center justify-center'>
           <Form />
         <div className='absolute top-4 right-4 cursor-pointer' onClick={()=>setOpen(false)}>
              {/* <Image src='/close.png' alt='' width={14} height={14} /> */}
