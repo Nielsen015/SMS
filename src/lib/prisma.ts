ec4f6f1ec8a07a4e0dbc91ea6e0308446dd87ae9
Moses Nielsen <mosesnielsen68@gmail.com>
2025-03-29 22:04:17 +0300

    Data base set up

---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+// declare prisma client variable
+const PrismaClientSingleton = () =>{
+    return new PrismaClient()
+}
+
+// check if prisma client exists if not create prisma client
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof PrismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? PrismaClientSingleton()
+
+export default prisma
+
+// If environment is not development, then we will use the prisma client from the environment variable
+if (process.env.NODE_ENV !=='production') globalThis.prismaGlobal = prisma
