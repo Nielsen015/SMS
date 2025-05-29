b8bdd6ac593745e184a7cc2adabf7c1044b4040c
Moses Nielsen <mosesnielsen68@gmail.com>
2025-05-29 23:52:26 +0300

    Calendar event configurations

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,14 @@
 import UserCard from "@/components/UserCard"
 import FinanceChart from "@/components/FinanceChart"
-import TermCalendar from "@/components/TermCalendar"
 import Annoucements from "@/components/Annoucements"
 import CountChartContainer from "@/components/CountChartContainer"
 import AttendanceChartContainer from "@/components/AttendanceChartContainer"
-const AdminPage = () => {
+import TermCalendarContainer from "@/components/TermCalendarContainer"
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: {[keys: string]: string | undefined}
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
@@ -34,7 +38,7 @@ const AdminPage = () => {
       </div>
       {/* Right */}
       <div className='w-full lg:w-1/3 flex flex-col gap-8'>
-      <TermCalendar />
+      <TermCalendarContainer searchParams={searchParams} />
       <Annoucements />
       </div>
     </div>
